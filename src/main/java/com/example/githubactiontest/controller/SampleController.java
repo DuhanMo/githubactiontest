89be2b17a8bd22c8c5d8e53eeab2e47eb3550960
Duhan Mo <moduhan95@gmail.com>
2022-12-21 21:17:25 +0900

    test controller

---
+++ src/main/java/com/example/githubactiontest/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.example.githubactiontest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
