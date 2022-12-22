9f38dec9271b20b9b39dec0b84789cef104c8eae
Duhan Mo <moduhan95@gmail.com>
2022-12-22 18:13:08 +0900

    show result failure test

---
+++ src/test/java/com/example/githubactiontest/unit/SampleTest.java
@@ -0,0 +1,14 @@
+package com.example.githubactiontest.unit;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SampleTest {
+
+    @Test
+    public void failure_test() {
+        assertThat(3+1).isEqualTo(1);
+    }
+}
